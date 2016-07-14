7eab1032e5c5cd16f2564965c24bf3d906b6faf1
ayork22 <agyork22@gmail.com>
2016-07-14 12:33:36 -0600

    Updates

--- RedisFP/src/apm/redis/Telnet.java
+++ RedisFP/src/apm/redis/Telnet.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -14,62 +17,203 @@ public class Telnet {
 	@SuppressWarnings({ "unchecked" })
 	public static void main(String[] args) throws IOException {
 
-		Socket sock = null;
-		PrintWriter out = null;
-		BufferedReader in = null;
+		Runnable servers = new Runnable() {
 
-		sock = new Socket("localhost", 6379);
-		out = new PrintWriter(sock.getOutputStream(), true);
-		in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+			public void run() {
 
-		out.println("INFO");
-		// StringBuilder sb = new StringBuilder();
-		ArrayList<String> data = new ArrayList<>();
-		JSONArray metricArray = new JSONArray();
-		// while ((data = in.readLine()) != null) {
-		while (data.add(in.readLine())) {
+				Socket sock = null;
+				PrintWriter out = null;
+				BufferedReader in = null;
 
-			// System.out.println(data.get(data.size() -1));
-			if (data.get(data.size() - 1).equals("# Keyspace"))
-				break;
-		}
-		out.close();
-		in.close();
-		sock.close();
-		// System.out.println("TEST***************");
-		for (String s : data) {
+				try {
+					sock = new Socket((GetPropertiesFile.getPropertyValue("RedisServer")), Integer.parseInt(GetPropertiesFile.getPropertyValue("RedisPort")));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					out = new PrintWriter(sock.getOutputStream(), true);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 
-			if (!s.contains(":"))
-				continue;
+				out.println("INFO");
+				// StringBuilder sb = new StringBuilder();
+				ArrayList<String> data = new ArrayList<>();
+				JSONArray metricArray = new JSONArray();
+				// while ((data = in.readLine()) != null) {
+				try {
+					while (data.add(in.readLine())) {
 
-			// System.out.println(s);
+						// System.out.println(data.get(data.size() -1));
+						if (data.get(data.size() - 1).equals("# Keyspace"))
+							break;
+					}
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				out.close();
+				try {
+					in.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					sock.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-			if (s.split(":")[0].equals("redis_version")) {
+				for (String s : data) {
 
-				System.out.println("MetricName = " + s.split(":")[0]);
-				String metricValue = s.split(":")[1];
-				System.out.println("MetricValue = " + metricValue);
+					if (!s.contains(":"))
+						continue;
 
-				// JSONArray metricArray = new JSONArray();
-				metricArray.add(createMetric("StringEvent", "Redis:" + s.split(":")[0], metricValue));
+					if (s.split(":")[0].equals("redis_version")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("StringEvent", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+
+					if (s.split(":")[0].equals("used_memory")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+
+					if (s.split(":")[0].equals("instantaneous_ops_per_sec")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+
+					if (s.split(":")[0].equals("evicted_keys")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+
+					if (s.split(":")[0].equals("mem_fragmentation_ratio")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("StringEvent", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+
+					if (s.split(":")[0].equals("connected_clients")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+					if (s.split(":")[0].equals("connected_slaves")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+					if (s.split(":")[0].equals("master_last_io_seconds_ago")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+					if (s.split(":")[0].equals("keyspace_misses")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+					if (s.split(":")[0].equals("total_commands_processed")) {
+
+						System.out.println("MetricName = " + s.split(":")[0]);
+						String metricValue = s.split(":")[1];
+						System.out.println("MetricValue = " + metricValue);
+
+						// JSONArray metricArray = new JSONArray();
+						metricArray.add(createMetric("LongCounter", "RedisTest1:" + s.split(":")[0], metricValue));
+					}
+
+				}
+				// System.out.println("TEST***************");
+				JSONObject metricsToEPAgent = new JSONObject();
+				metricsToEPAgent.put("metrics", metricArray);
+				System.out.println("All Metrics = " + metricsToEPAgent.toString());
+				WebServiceHandler.sendMetric(metricsToEPAgent.toString(), (GetPropertiesFile.getPropertyValue("EPAgentHost")),Integer.parseInt(GetPropertiesFile.getPropertyValue("EPAgentPort")) );
 			}
 
+			public JSONObject createMetric(String type, String name, Object value) {
+
+				JSONObject metric = new JSONObject();
+				metric.put("type", type);
+				metric.put("name", name);
+				metric.put("value", value);
+				System.out.println("metricINFO = " + metric.toString());
+				return metric;
+			}
+		};
+
+		ScheduledExecutorService executor = Executors.newScheduledThreadPool(2);
+		// executor.scheduleAtFixedRate(servers, 0, 15, TimeUnit.SECONDS);
+		if ((GetPropertiesFile.getPropertyValue("delaytime").isEmpty())
+				|| Integer.parseInt(GetPropertiesFile.getPropertyValue("delaytime")) < 5) {
+			executor.scheduleAtFixedRate(servers, 0, 5, TimeUnit.MINUTES);
+
+		} else {
+			executor.scheduleAtFixedRate(servers, 0, Integer.parseInt(GetPropertiesFile.getPropertyValue("delaytime")),
+					TimeUnit.MINUTES);
+
+			// }
+			// Loop to keep Main Thread alive
+			while (true) {
+				try {
+					Thread.sleep(15000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
 		}
-		// System.out.println("TEST***************");
-		JSONObject metricsToEPAgent = new JSONObject();
-		metricsToEPAgent.put("metrics", metricArray);
-		System.out.println("All Metrics = " + metricsToEPAgent.toString());
-		WebServiceHandler.sendMetric(metricsToEPAgent.toString(), "localhost", 9080);
-	}
-
-	@SuppressWarnings("unchecked")
-	public static JSONObject createMetric(String type, String name, Object value) {
-
-		JSONObject metric = new JSONObject();
-		metric.put("type", type);
-		metric.put("name", name);
-		metric.put("value", value);
-		System.out.println("metricINFO = " + metric.toString());
-		return metric;
 	}
 }
